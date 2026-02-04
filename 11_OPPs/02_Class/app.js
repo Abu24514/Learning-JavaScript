402e075b928fc1cd746976917c9d2042c6d65165
Abu Hanjala <abuhanjala656@gmail.com>
2026-02-04 12:32:38 +0530

    class

---
+++ 11_OPPs/02_Class/app.js
@@ -0,0 +1,13 @@
+class Toffee {
+  //  har class banate hi pahle chiz jo banate hai uski aadat daal lo uska naam hai constructor.
+  constructor(flavor , color, taste , shape) {
+    this.flavor = flavor;
+    this.color = color;
+    this.taste = taste;
+    this.shape = shape;
+  }
+}
+
+ const t1 = new Toffee ("chocolate" , "brown" , "sweet" , "round");
+ const t2 = new Toffee ("vanilla" , "white" , "sweet" , "square");
+ const t3 = new Toffee ("straberry" , "pink" , "sweet" , "heart");
